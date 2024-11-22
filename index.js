60f9a991a75e82608162e9398212fa99f8cad995
Andrei <84713428+andrewmat32@users.noreply.github.com>
2024-11-22 15:07:00 +0200

    - added js functions for filters toggle on mobile

--- index.js
+++ index.js
@@ -38,3 +38,28 @@ function topFunction()
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function moveFiltersToMobile(el) {
+    const filters = document.getElementById('product-filters');
+    const mobileFilters = document.getElementById('product-filters-mobile');
+    while (filters.firstChild) {
+        mobileFilters.appendChild(filters.firstChild);
+    }
+    el.onclick = function () {
+        moveFiltersBack(el);
+    };
+    el.textContent = "Close Filters"; // Update button text
+}
+
+function moveFiltersBack(el) {
+    const filters = document.getElementById('product-filters');
+    const mobileFilters = document.getElementById('product-filters-mobile');
+    while (mobileFilters.firstChild) {
+        filters.appendChild(mobileFilters.firstChild);
+    }
+    el.onclick = function () {
+        moveFiltersToMobile(el);
+    };
+    el.textContent = "Open Filters"; // Update button text
+}
+
